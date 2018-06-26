1129516711f5503d0efe39189ce48579d433671f
Stephen Khuu <skhuu@connectedlab.com>
2018-06-26 18:41:27 -0400

    Added sample response

--- app/routes/theme_routes.js
+++ app/routes/theme_routes.js
@@ -2,9 +2,16 @@
 module.exports = function(app, db) {
     app.post('/themebot', (req, res) => {
         console.log('=====');
-        console.log(res.text);
+        console.log(req.body.text);
         console.log('=====');
         // You'll create your note here.
-        res.send('Hello')
+        //res.send('Hello');
+
+        let data = {
+            response_type: 'in_channel', // public to the channel
+            text: '#F8F8FA,#F8F8FA,#1266fb,#FFFFFF,#FFFFFF,#383F45,#60D156,#DC5960'
+        };
+          
+        res.json(data);
     });
 };
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const app            = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const port = (process.env.PORT || 8000);
 
 require('./app/routes')(app, {});
