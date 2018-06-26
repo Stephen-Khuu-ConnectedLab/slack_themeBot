ca83deefddea01a69ea9039cbb3b2e877900843a
Stephen Khuu <skhuu@connectedlab.com>
2018-06-26 18:19:48 -0400

    Add request debugger

--- app/routes/theme_routes.js
+++ app/routes/theme_routes.js
@@ -1,8 +1,9 @@
 // routes/theme_routes.js
 module.exports = function(app, db) {
     app.post('/themebot', (req, res) => {
-        console.log(req);
-        console.log(res);
+        console.log('=====');
+        console.log(res.text);
+        console.log('=====');
         // You'll create your note here.
         res.send('Hello')
     });
